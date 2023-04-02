7fcb4579542d9a168c322b41fcaf4fe12ed38fdf
itzshakeeb <itzshakeeb@gmail.com>
2023-04-02 19:59:36 +0600

    add loading

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,30 +1,32 @@
-
-
 export const getUser = async (email) => {
     const res = await fetch(`http://localhost:3210/api/v2/users?email=${email}`)
     const data = await res.json()
     return data.data[0]
 }
+//Category functions :
 
 export const getCatagories = async () => {
     const res = await fetch(`http://localhost:3210/api/v2/catagories`)
     const data = await res.json()
     return data.data
 }
+export const getShopByCategory = async (id) => {
+    const res = await fetch(`http://localhost:3210/api/v2/products/${id}`)
+    const data = await res.json()
+    return data.data
+}
 
+//: shops functions :
 export const getUserShops = async (email) => {
     const res = await fetch(`http://localhost:3210/api/v2/shops/${email}`)
     const data = await res.json()
     return data.data
 }
-
 export const getShopsByUser = async (email) => {
     const res = await fetch(`http://localhost:3210/api/v2/shops/${email}`)
     const data = await res.json()
     return data.data
 }
-
-
 export const pendingShops = async () => {
     const res = await fetch(`http://localhost:3210/api/v2/shops/type/pending`)
     const data = await res.json()
@@ -40,10 +42,4 @@ export const verifiedShopsAPI = async (limit) => {
         const data = await res.json()
         return data.data
     }
-
-}
-export const getShopByCategory = async (id) => {
-    const res = await fetch(`http://localhost:3210/api/v2/products/${id}`)
-    const data = await res.json()
-    return data.data
 }
--- src/Components/Home Components/CatagorysSection.js
+++ src/Components/Home Components/CatagorysSection.js
@@ -7,27 +7,32 @@ import PrimaryLoading from '../LoadingSpin/PrimaryLoading';
 
 const CatagoriesSection = () => {
 
-    const { data: catagories = [], isLoading, refetch } = useQuery({
+    const { data: catagories = [], isLoading, refetch, isFetching, isInitialLoading } = useQuery({
         queryKey: ['catagories'],
         queryFn: getCatagories,
     })
-    if (isLoading) return <div className="flex justify-center items-center w-full h-[200px]">
+    if (isLoading || isFetching || isInitialLoading) return <div className="flex justify-center items-center w-full h-[200px]">
         <PrimaryLoading />
     </div>
 
-    refetch()
     return (
         <div className="container px-3 md:px-9 m-auto mb-5 ">
-            <div className="grid grid-cols-5 md:grid-cols-5 gap-1 lg:gap-4  group">
-                {catagories?.map((data, i) =>
-                    <Link to={`/shops/category/${data?._id}`} key={i}>
-                        <div className="cursor-pointer group-hover:blur-sm  
+            {isInitialLoading || isFetching || isLoading ?
+                <div className="flex justify-center items-center w-full h-[200px]">
+                    <PrimaryLoading />
+                </div>
+                :
+
+                <div className="grid grid-cols-5 md:grid-cols-5 gap-1 lg:gap-4  group">
+                    {catagories?.map((data, i) =>
+                        <Link to={`/shops/category/${data?._id}`} key={i}>
+                            <div className="cursor-pointer group-hover:blur-sm  
               hover:!blur-none group-hover:scale-[0.92] hover:!scale-100  duration-300" key={i}>
-                            <img src={data.PhotoUrl} height="300px" alt="" />
-                        </div>
-                    </Link>
-                )}
-            </div>
+                                <img src={data.PhotoUrl} height="300px" alt="" />
+                            </div>
+                        </Link>
+                    )}
+                </div>}
         </div>
     );
 };
